54c2acd7545cd3df98035b9a2b11c86694409286
Hakan Uyumaz <hakanuyumaz@gmail.com>
2015-09-03 16:38:27 +0300

    Home page layout edited

--- base_communicator/views/main_views.py
+++ base_communicator/views/main_views.py
@@ -3,7 +3,7 @@ __author__ = 'Hakan Uyumaz'
 from django.shortcuts import render, redirect
 from django.db.models import Q
 
-from base_communicator.models import Journey, Friendship
+from base_communicator.models import Journey, Friendship, User
 
 
 def main_page_view(request):
@@ -20,8 +20,16 @@ def home_page_view(request):
         user.followings = Friendship.objects.filter(owner=user, type='Fo')
         user.friends = Friendship.objects.filter((Q(owner=user) | Q(friend=user)) & Q(type='Fr'))
         user.journeys = Journey.objects.filter(owner=request.user)
-        print(str(user.educations.all()))
         return render(request, "home.html",
                       {"user": request.user})
+    else:
+        return redirect('main')
+
+
+def search_page_view(request):
+    key = request.GET["key"]
+    if request.user.is_authenticated():
+        users = User.objects.filter(Q(name=key) | Q(surname=key))
+        return render(request, "search.html", {"users": users})
     else:
         return redirect('main')
