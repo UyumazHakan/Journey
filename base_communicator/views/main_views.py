7326f94036b7bc3417808831915cc966713a5a6e
Hakan Uyumaz <hakanuyumaz@gmail.com>
2015-09-04 16:01:52 +0300

    Journeys page second phase and profile page with journeys first phase

--- base_communicator/views/main_views.py
+++ base_communicator/views/main_views.py
@@ -3,7 +3,7 @@ __author__ = 'Hakan Uyumaz'
 from django.shortcuts import render, redirect
 from django.db.models import Q
 
-from base_communicator.models import Journey, Friendship, User
+from base_communicator.models import Friendship, User
 
 
 def main_page_view(request):
@@ -19,9 +19,9 @@ def home_page_view(request):
         user.followers = Friendship.objects.filter(friend=user, type='Fo')
         user.followings = Friendship.objects.filter(owner=user, type='Fo')
         user.friends = Friendship.objects.filter((Q(owner=user) | Q(friend=user)) & Q(type='Fr'))
-        user.journeys = Journey.objects.filter(owner=request.user)
+        journeys = user.owned_journeys.all()
         return render(request, "home.html",
-                      {"user": request.user})
+                      {"user": request.user, "journeys": journeys})
     else:
         return redirect('main')
 
