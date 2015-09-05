a4944a0c847aafaebaa82bc8ef14a6c30526d480
Hakan Uyumaz <hakanuyumaz@gmail.com>
2015-09-05 16:06:16 +0300

    Journeys page second phase and profile page with journeys first phase

--- base_communicator/views/friend_views.py
+++ base_communicator/views/friend_views.py
@@ -1,8 +1,8 @@
 __author__ = 'Hakan Uyumaz'
 
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
 
-from base_communicator.models import User, FriendshipRequest
+from base_communicator.models import User, FriendshipRequest, Friendship
 
 
 def add_friend(request):
@@ -18,3 +18,34 @@ def add_friend(request):
             friendship_request.status = 'P'
             friendship_request.save()
     return redirect('main')
+
+
+def follower_page_view(request, user_id):
+    if request.user.is_authenticated():
+        try:
+            user = User.objects.get(public_id=user_id)
+        except User.DoesNotExist:
+            return redirect("main")
+        try:
+            follower = Friendship.objects.get(owner=user)
+        except Friendship.DoesNotExist:
+            follower = []
+        return render(request, 'friends_list.html', {"users": follower})
+    else:
+        return redirect('main')
+
+
+def following_page_view(request, user_id):
+    if request.user.is_authenticated():
+        try:
+            user = User.objects.get(public_id=user_id)
+        except User.DoesNotExist:
+            return redirect("main")
+        try:
+            following = Friendship.objects.get(owner=user)
+        except Friendship.DoesNotExist:
+            following = []
+
+        return render(request, 'friends_list.html', {"users": following})
+    else:
+        return redirect('main')
